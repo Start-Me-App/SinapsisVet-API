37957a161fede135d5831d6650cf67072da8607e
Ezequiel Manzano <68036282+ezemanzano@users.noreply.github.com>
2024-12-16 12:09:54 -0300

    fix

--- app/app/Http/Controllers/CoursesController.php
+++ app/app/Http/Controllers/CoursesController.php
@@ -440,7 +440,7 @@ class CoursesController extends Controller
     
             $list = Courses::with(['category','professors','lessons.professor','workshops'])
             ->select('courses.*', 'inscriptions.id as inscribed')
-            ->leftJoin('inscriptions', 'courses.id', '=', 'inscriptions.course_id')
+            ->rightJoin('inscriptions', 'courses.id', '=', 'inscriptions.course_id')
             ->where('courses.active', 1)
             #where user is inscribed or user_id is null
             ->where(function($query) use ($user) {
